36f98da6b77fa51d92d42f4ddd474746b951e14b
Nitin Tony Paul <108007300+nitintonypaul@users.noreply.github.com>
2025-05-28 21:12:10 +0530

    uploading egarch module (incomplete)

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # Importing Dependencies
 import argparse
 import yfinance as yf
+import sys
+
+# Custom modules 
 from py_modules.data_handler import compute_elements
 
 # Argument object
@@ -17,8 +20,14 @@ args = parser.parse_args()
 stocks = args.stock
 investments = args.investment
 
+# Checking whether each stock has an investment
+# Displaying message and exiting with 0 status
+if len(stocks) != len(investments):
+    print("Every stock does not have a corresponding investment. Please Try again.")
+    sys.exit(0)
+
 '''
-Stuff to obtain include and mean (for EGARCH and JDM)
+Stuff to obtain:  mean (for JDM)
 Optimization data not yet identified
 '''
 
@@ -32,7 +41,7 @@ for i in stocks:
     current_price = ticker_obj.history(period="1d")["Close"].iloc[-1]
 
     # Obtaining volatility before 180d
-    data = ticker_obj.history(period="210d")
+    data = ticker_obj.history(period="211d")
     prices = data["Close"][0:30]
 
     # Returns array to compute shock value
