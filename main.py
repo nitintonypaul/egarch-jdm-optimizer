0f6fcec0c3e855159a4c6fe8d1227c0a8a6bcfe2
Nitin Tony Paul <108007300+nitintonypaul@users.noreply.github.com>
2025-05-30 23:59:48 +0530

    adding merton module

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 import argparse
 import yfinance as yf
 import sys
+import math
+import numpy as np
 
 # Custom modules 
-from py_modules.data_handler import compute_elements
+from py_modules.data_handler import compute_elements, compute_mean
 from egarch import estimate
+from merton import simulate
 
 # Argument object
 parser = argparse.ArgumentParser(description="Process stocks and investments")
@@ -31,16 +34,18 @@ if len(stocks) != len(investments):
     print("Every stock does not have a corresponding investment. Please Try again.")
     sys.exit(0)
 
-'''
-Stuff to obtain:  mean (for JDM)
-Optimization data not yet identified
-'''
+# Defining time in terms of trading years
+time = 1/252
 
 # For each stock in stocks
-for i in stocks:
+for i in range(len(stocks)):
+
+    # Obtaining stocks and corresponding investment
+    stock = stocks[i]
+    investment = investments[i]
 
     # Defining ticker object
-    ticker_obj = yf.Ticker(i)
+    ticker_obj = yf.Ticker(stock)
 
     # Obtaining current price of the stock
     current_price = ticker_obj.history(period="1d")["Close"].iloc[-1]
@@ -49,14 +54,65 @@ for i in stocks:
     data = ticker_obj.history(period="211d")
     prices = data["Close"][0:30]
 
+    # Obtaining jump returns from jump data taken over a period of 1 year
+    jumpdata = ticker_obj.history(period="1y")
+    jump_prices = jumpdata["Close"]
+    jump_returns = jump_prices.pct_change().dropna()
+
     # Returns array to compute shock value
     returns_array = data["Close"][30:]
+
+    # Computing mean for JDM
+    mean = compute_mean(returns_array)
 
     # Obtaining volatility and shock array
     vol, shock_array = compute_elements(prices, returns_array)
 
     # Computing expected volatility using EGARCH
     expected_volatility = estimate(len(shock_array),vol,shock_array)
+
+    # Computing threshold for jumps
+    threshold = 3 * jump_returns.std()
+
+    # Obtaining jumps in fractions
+    jumps = jump_returns[abs(jump_returns) > threshold]
+    jumps = jumps.tolist()
+
+    # Obtaining average jump (Logarithmic)
+    ksum = 0
+    for i in jumps:
+        ksum+=math.log(1+i)
     
+    # Considering case if jumps array is empty
+    if len(jumps) == 0:
+        k = 0
+    else:
+        k = ksum/len(jumps)
+
+    # Computing jump volatility
+    jump_vol_array = []
+    for i in jumps:
+        jump_vol_array.append(math.log(1+i))
+    
+    # Considering case if jump_vol_array is empty
+    if len(jump_vol_array) == 0:
+        sig_j = 0
+    else:
+        sig_j = np.std(jump_vol_array, ddof=1)
+
+    # Computing Average jump frequency over given time
+    lambda_ = len(jumps)/(1/time)
+
+    # Annualizing mean and volatility before plugging into JDM
+    mean = mean * 252
+    expected_volatility = expected_volatility * math.sqrt(252)
+
+    # Simulating prices using MERTON
+    expected_price = simulate(current_price, mean, expected_volatility, lambda_, k, sig_j, time)
+    
+    print(f"Invested amount: {investment}")
+    print(f"Current Stock price of {stock}: {current_price}")
+    print(f"Expected Stock price of {stock}: {expected_price}")
+    print(f"Expected return: {(investment/current_price)*expected_price}")
     print(expected_volatility)
     
